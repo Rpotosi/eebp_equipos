afff704a3c9bc08853ca0a63e0c7c1d834b5a89d
Rpotosi <rpotosi@estudiantes.areandina.edu.co>
2023-10-14 17:07:08 -0500

    Generate Model and Controller administrativo, Distribucion. SST

---
+++ app/Http/Controllers/DistribucionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Distribucion;
+use Illuminate\Http\Request;
+
+class DistribucionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Distribucion $distribucion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Distribucion $distribucion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Distribucion $distribucion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Distribucion $distribucion)
+    {
+        //
+    }
+}
