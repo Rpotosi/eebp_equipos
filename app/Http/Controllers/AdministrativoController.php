fc6272ba344467be25c9f6a78664b4e4d0f3b636
rpotosi <rpotosi@estudiantes.areandina.edu.co>
2023-10-26 18:06:15 -0500

    model and migration version 2

--- app/Http/Controllers/AdministrativoController.php
+++ app/Http/Controllers/AdministrativoController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\AdministrativoController;
-
 
 use App\Models\Administrativo;
 use Illuminate\Http\Request;
+
 
 class AdministrativoController extends Controller
 {
