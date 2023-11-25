984a59168d60b751ea3fd9b45042128ef88fed93
Rpotosi <rpotosi@estudiantes.areandina.edu.co>
2023-11-25 18:07:21 -0500

    Login funcional

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Home;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -13,8 +14,15 @@ class HomeController extends Controller
      */
     public function show() // Se deja vacio porque no recibe datos
     {
-        return view('Home.home');
+        $user = $this->getCurrentUser();
+        // Pasar el usuario a la vista home.home-show
+        return view('Home.home', compact('user'));
     }
 
-
+    private function getCurrentUser()
+    {
+        // Busca y devuelve el usuario actual basado en su id
+        return User::find(Auth::user()->id);
+    }
 }
+
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,40 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\LoginRequest;
+
 
 class LoginController extends Controller
 {
-    //
+
+    /*****************************************************
+     Método para mostrar la vista de la página de Login
+    *****************************************************/
     public function show()
     {
+        // Verificar si el usuario ya está autenticado
         if(Auth::check()){
-            return redirect()->route('home.show');
+            // Redirigir al usuario a la página de inicio si ya está autenticado
+            return redirect('home.show');
         }
-        return view('Login.Login');
+        // Mostrar la vista de inicio de sesión
+        return view('login.login');
     }
 
-    public function login(LoginRequest $request)
-    {
+    /*****************************************************
+     Método para procesar el inicio de sesión del usuario
+    *****************************************************/
+    public function login(LoginRequest $request){
+
+        // Obtener las credenciales de inicio de sesión del formulario
         $credentials = $request->getCredentials();
-        
-        if(!Auth::validate($credentials)):
-            dd('error');
-           return redirect()->to('login')
-                ->withErrors(trans('auth.failed'));
-        endif;
+
+        // Validar las credenciales proporcionadas
+        if(!Auth::validate($credentials)){
+            // Mostrar una alerta de error si las credenciales no son válidas
+            session()->flash('error','Usuario o contraseña incorrectos.');
+            return redirect()->to('/');
+        }
+    
+        // Recuperar y almacenar el usuario basado en las credenciales
         $user = Auth::getProvider()->retrieveByCredentials($credentials);
-        
-
+    
+        // Iniciar sesión con el usuario autenticado
         Auth::login($user);
-
-        return $this->authenticated($request, $user);
+    
+        // Retorna el método autenticate
+        return $this->authenticate($request, $user);
     }
 
-    protected function authenticated(Request $request, $user) 
-    {
-        return redirect()->route('home.show');
+    /*****************************************************
+     Método redirigir al usuario a la página de inicio autenticada
+    *****************************************************/
+    public function authenticate(Request $request, $user){
+        // Redirigir al usuario a la página de inicio autenticada
+        return redirect('home');
     }
 }
